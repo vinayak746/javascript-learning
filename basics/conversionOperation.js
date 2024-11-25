278bb25339b78f2f968b557b4d1feb0cb4349d9d
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2024-11-25 12:51:41 +0000

    conversionOperations

---
+++ basics/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true =>1; false =>0
+
+let isLoggedIN =1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// ""=> false
+// "vinayak" => true
+ 
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
