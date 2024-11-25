9135092ef97e4d81eec91eebdba0ba62386ac69a
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2024-11-25 13:19:49 +0000

    conversionOperations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -21,3 +21,20 @@ let someNumber = 33
 let stringNumber= String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber)
+
+// ***********************************************Operations**********************************************
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " vinayak"
+let str3 = str1 +str2
+// console.log(str3)
+
+// console.log("1"+2) // 12
+// console.log(1+"2") // 12
+// console.log("1"+2+2) // 122
+// console.log(1+2+"2") // 32
+ 
