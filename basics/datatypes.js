463b57950c8d9a81929dbb2aea7cb5dff3a890d4
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2024-11-25 12:51:18 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat alll JS code as newer version
+
+// alert(3+3) we are using nodeJS , not browser
+
+console.log(3+3)
+
+let name = "vinayak"
+
+let age = 18
+let isLoggedIN= true
+ 
+// number =>2 to power 53
+// bigint
+// string=> ""
+// bollean => true/false
+// null =>standalone value
+// undefined => 
+// symbol =>unique
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
