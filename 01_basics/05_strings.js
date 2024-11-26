50c94ff6d47123138b772aa73a914858fd8c13a8
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2024-11-26 10:36:01 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "vinayak"
+const repoCount = 50 
+
+// console.log(name + repoCount + " value"); // dont use this 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // right way to write the strings 
+
+const gameName = new String('VinayakVA')
+
+console.log(gameName[0]);// it will print V
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('V'));
+
+
+
+
+
+
