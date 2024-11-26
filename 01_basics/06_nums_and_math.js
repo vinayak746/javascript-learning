7f958c7e89887c5bd28280d0ac6596a19a2e2ef0
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2024-11-26 11:04:53 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 500
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.565
+
+//console.log(otherNumber.toPrecision(4)); // it precies the value like in this it will give us 123.6 
+// and it the no. is 3 it will give us 124
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN')); // add commas accoriding to indian system if we include en-IN
+
+
+//+++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-5));// convert negative to positive 
+// console.log(Math.round(5.6));// round the value
+// console.log(Math.floor(4.2));// lower value 
+// console.log(Math.ceil(4.2));// upper value
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random()*(max-min +1)+ min));
+
+
+
+
+
+ 
+
+
+
+
